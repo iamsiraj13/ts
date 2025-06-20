59afe414106bdb0f5107270c9f398c988a4de3c6
iamsiraj13 <iamsiraj13@gmail.com>
2025-06-20 06:56:59 +0600

    init

--- src/js/classes.ts
+++ src/js/classes.ts
@@ -1,12 +1,12 @@
 import { Player } from "../classes/Player.js"
 
-const sakib = new Player('Sakib',34,'bangladesh')
-const mashrafi = new Player('mashrafi',34,'bangladesh')
+const sakib = new Player('Sakib', 34, 'bangladesh')
+const mashrafi = new Player('mashrafi', 34, 'bangladesh')
 
 console.log(sakib.name)
 console.log(sakib.age)
 
-const players:Player[]=[]
+const players: Player[] = []
 
 players.push(sakib)
 players.push(mashrafi)
