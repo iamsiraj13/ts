2f281481e18fa84af9a42325125310932355f6ac
iamsiraj13 <iamsiraj13@gmail.com>
2025-06-20 22:25:28 +0600

    class blueprint type

--- output/classes/Player.js
+++ output/classes/Player.js
@@ -1,10 +1,18 @@
-export class Player {
+"use strict";
+class Player {
     constructor(n, a, c) {
         this.name = n;
         this.age = a;
         this.country = c;
     }
     play() {
-        console.log(`${name} is from ${this.country}`);
+        console.log(`${this.name}  from ${this.country} is playing`);
     }
 }
+const mashrafi = new Player('Mashrafi', 40, 'bangladesh');
+const sakib = new Player('sakib', 39, 'bangladesh');
+console.log(mashrafi.name);
+const players = [];
+players.push(mashrafi);
+players.push(sakib);
+console.log(players);
--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -1,16 +1,27 @@
-export class Player{
-    public name:string;
-    public age:number;
-    public country:string;
+ class Player{
+    name:string;
+    age:number;
+    country:string;
 
     constructor(n:string, a:number, c:string){
-        this.name = n
-        this.age = a
+        this.name = n;
+        this.age = a;
         this.country = c
-
     }
-    
+
     play(){
-        console.log(`${name} is from ${this.country}`)
+        console.log(`${this.name}  from ${this.country} is playing`)
     }
 }
+
+
+const mashrafi = new Player('Mashrafi',40,'bangladesh')
+const sakib = new Player('sakib',39,'bangladesh')
+
+console.log(mashrafi.name)
+const players:Player[] = []
+
+players.push(mashrafi)
+players.push(sakib)
+
+console.log(players)
